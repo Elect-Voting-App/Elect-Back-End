9f15ccb2c17a29aa03be8bb67ee29e962d61b5ef
Emmanuel Adjei-Sah <emmanueladjei-sah@JARVIS-MacBook-Pro.local>
2020-05-23 17:01:28 +0000

    + Started building the server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+//Middleware 
+app.use(express.urlencoded({ extended: false }));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
